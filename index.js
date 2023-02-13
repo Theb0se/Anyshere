9636ccc4fce6811f56393cdf1d60e300e9e8baee
Rishabh Bose <Rishabhbose3@gmail.com>
2023-02-13 10:16:56 +0530

    port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + "/public"));
 
 const upload = multer({ dest: "uploads" });
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.render("index", { filelink: null }));
 
